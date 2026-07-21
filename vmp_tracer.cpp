bf585fbaa957f94abba21920f33ef0cf2a45affe
yeshengmogu <3332720895@proton.me>
2026-07-21 22:22:47 +0800

    full dispatch trace

--- vmp_tracer.cpp
+++ vmp_tracer.cpp
@@ -4,36 +4,28 @@
 FILE* trace;
 PIN_LOCK traceLock;
 UINT64 totalHits = 0;
+UINT64 maxHits = 50000;  // 限制记录量
 
-// Dispatch loop addresses (key instructions)
-static UINT32 dispatchAddrs[] = {
-    0x46845ab,  // add eax, 0x4350ab0 (start of dispatch)
-    0x46845ba,  // ret (jump to handler)
-    0x46845fb,  // mov [edi+eax], ecx (write to table)
-    0x4684603,  // mov eax, [esi] (read bytecode)
-    0x468460f,  // ror eax, 0x17 (decrypt)
-    0x468461d,  // sub ebx, eax (modify EBX)
-    0x4684622,  // lea eax, [eax+0x204774de] (compute next handler)
-    0x468462c,  // jmp 0x46845ab (loop)
-};
-#define NUM_DISPATCH (sizeof(dispatchAddrs)/sizeof(dispatchAddrs[0]))
+// Dispatch loop region
+#define DISPATCH_START 0x46845ab
+#define DISPATCH_END   0x4684630
 
-VOID RecordDispatch(VOID* ip, CONTEXT* ctx)
+VOID RecordInsn(VOID* ip, CONTEXT* ctx)
 {
     PIN_GetLock(&traceLock, 1);
     totalHits++;
     
-    UINT32 rip = (UINT32)(ADDRINT)ip;
-    fprintf(trace, "%x eax=%x ebx=%x ecx=%x edx=%x esi=%x edi=%x esp=%x ebp=%x\n",
-            rip,
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EAX),
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EBX),
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_ECX),
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EDX),
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_ESI),
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EDI),
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_ESP),
-            (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EBP));
+    if (totalHits <= maxHits) {
+        UINT32 rip = (UINT32)(ADDRINT)ip;
+        fprintf(trace, "%x eax=%x ebx=%x ecx=%x edx=%x esi=%x edi=%x\n",
+                rip,
+                (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EAX),
+                (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EBX),
+                (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_ECX),
+                (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EDX),
+                (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_ESI),
+                (UINT32)PIN_GetContextReg(ctx, LEVEL_BASE::REG_EDI));
+    }
     
     PIN_ReleaseLock(&traceLock);
 }
@@ -43,11 +35,9 @@ VOID Instruction(INS ins, VOID* v)
     ADDRINT addr = INS_Address(ins);
     UINT32 a = (UINT32)addr;
     
-    for (int i = 0; i < NUM_DISPATCH; i++) {
-        if (a == dispatchAddrs[i]) {
-            INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)RecordDispatch, IARG_INST_PTR, IARG_CONTEXT, IARG_END);
-            return;
-        }
+    // Only trace dispatch loop region
+    if (a >= DISPATCH_START && a < DISPATCH_END) {
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)RecordInsn, IARG_INST_PTR, IARG_CONTEXT, IARG_END);
     }
 }
 
@@ -58,7 +48,7 @@ VOID Fini(INT32 code, VOID* v) {
 
 int main(int argc, char* argv[])
 {
-    trace = fopen("vmp_dispatch_trace.out", "w");
+    trace = fopen("vmp_full_trace.out", "w");
     if (!trace) return 1;
     PIN_InitLock(&traceLock);
     if (PIN_Init(argc, argv)) return -1;
